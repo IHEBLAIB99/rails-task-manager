4a6aade42cf7efa9c8b725c9d2eae0cdebe0bf0e
iheb laib <bba3at@gmail.com>
2024-01-31 20:27:54 +0000

    added the DELETE button

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,4 +21,15 @@ class TasksController < ApplicationController
   def edit
     @tasks = Task.find(params[:id])
   end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
 end
